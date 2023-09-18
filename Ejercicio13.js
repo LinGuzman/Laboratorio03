069bd52ce3df9ceef20ffdd1ada31671a739360c
Lin <lin.guzmantecsup.edu.pe>
2023-09-17 22:42:39 -0500

    laboratorio terminado

---
+++ Ejercicio13.js
@@ -0,0 +1,8 @@
+let vacaciones =false, diaDescanso = true;
+if( vacaciones || diaDescanso){
+    console.log("padre puede asistir al juego del hijo");
+
+}
+else{
+    console.log("El padre esta ocupado");
+}
